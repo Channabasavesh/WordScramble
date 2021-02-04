1a199776fefbe2aa91aa3ea15f053aec96cb0cfd
Channabasavesh Chandrappa Budihal <cbudihal@apple.com>
2021-02-04 19:43:21 +0530

    Initial Commit
    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Channabasavesh Chandrappa Budihal on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
